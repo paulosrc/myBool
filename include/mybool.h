f1b333265c8614ea96ce152e81bbaf91029fb796
paulosrc <joaopsc2002@gmail.com>
2025-02-18 00:23:24 -0300

    Correção de BIT_NOT e adição de LOGIC_NOT

--- include/mybool.h
+++ include/mybool.h
@@ -3,7 +3,7 @@ typedef _Bool bool; // Definição do tipo bool
 #define  true 1     // Definição de true como 1
 #define false 0     // Definição de false como 0
 
-#define   BIT_NOT(x)       !(x)            // Operação bitwise NOT
+#define   BIT_NOT(x)       ~(x)            // Operação bitwise NOT
 #define   BIT_AND(x, y)    ((x) & (y))     // Operação bitwise AND
 #define    BIT_OR(x, y)    ((x) | (y))     // Operação bitwise OR
 #define   BIT_XOR(x, y)    ((x) ^ (y))     // Operação bitwise XOR
@@ -11,6 +11,7 @@ typedef _Bool bool; // Definição do tipo bool
 #define   BIT_NOR(x, y)    !((x) | (y))    // Operação bitwise NOR
 #define  BIT_NXOR(x, y)    !((x) ^ (y))    // Operação bitwise NXOR
 
+#define LOGIC_NOT(x)       (!x)            // Operação lógica NOT
 #define LOGIC_AND(x, y)    ((x) && (y))    // Operação lógica AND
 #define  LOGIC_OR(x, y)    ((x) || (y))    // Operação lógica OR
 
